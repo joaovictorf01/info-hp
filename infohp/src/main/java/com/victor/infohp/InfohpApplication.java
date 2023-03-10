c52e29be2010490d6a886f89f86193c53fa6ed5f
João Victor <joaovictorf01@gmail.com>
2023-03-09 21:58:16 -0300

    início do projeto.

---
+++ infohp/src/main/java/com/victor/infohp/InfohpApplication.java
@@ -0,0 +1,13 @@
+package com.victor.infohp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InfohpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InfohpApplication.class, args);
+	}
+
+}
